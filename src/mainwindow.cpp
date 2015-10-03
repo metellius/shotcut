d33a4d2cc54d6e91477125d8565667609dc55ff9
Harald Hvaal <harald.hvaal@gmail.com>
2015-10-03 20:58:39 +0200

    Refactor Database to keep sql operations in single thread
    All database operations will now post a job request for the database thread,
    then block until it is completed. Testing shows that in practice there is
    rarely waiting going on due to multiple jobs in the queue.
    The most expensive operation of all: writing to disk, has been delayed until 5
    secs after the last database request, and all operations are grouped in an
    implicit transactions. This improves thumbnail loading time greatly as the disk
    is not written to before thumbnails are ready and on screen, and more
    importantly, only written to once (not one time for every thumbnail in the
    timeline).

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1607,6 +1607,7 @@ void MainWindow::closeEvent(QCloseEvent* event)
         if (!m_htmlEditor || m_htmlEditor->close()) {
             writeSettings();
             event->accept();
+            emit aboutToShutDown();
             QApplication::exit(m_exitCode);
             return;
         }
